2e0a85a2829151f30d653de31e8c9db902cac7dd
kaique roberto <kaike.roberto.kaike@gmail.com>
2023-03-30 19:57:25 -0300

    adicionado programa novo_ vetores

---
+++ tabuada/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+  int n, i, resultado;
+
+      printf("Deseja a tabuada para qual valor? ");
+      scanf("%d", &n);
+
+      for (i=1; i<=10; i++){
+        resultado = n * i;
+        printf("%d x %d = %d\n", n, i, resultado);
+      }
+
+
+
+    return 0;
+}
